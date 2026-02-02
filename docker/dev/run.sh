81f8fb6bdd5a4776ae28e0552796b482e0c89599
Michał Pajda <michal.pajda2002@gmail.com>
2026-02-02 17:53:46 +0100

    add release build

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'what' &&
 (cd backend && cargo watch -x "run") &
 (cd menu_front && trunk watch) &
 (cd test_front && trunk watch)
---
+++ run-release.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose -f ./docker/release/docker-compose.yml up --build
